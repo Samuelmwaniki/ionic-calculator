c905ee9069a0a9befcd386ff8223b9a9845f5722
Samuelmwaniki <smwaniki549@gmail.com>
2024-01-27 19:57:33 +0300

    ionic angular culaculator to compute basic arithmetic operations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+// string to display buttons with numbers on screen
+  display: string = '';
+  buttons: string[] = ['1', '2', '3', '/', '4', '5', '6', '*', '7', '8', '9', '-', 'C', '0', '+','=', ];
 
-  constructor() {}
-
+  //
+  onButtonClick(button: string) {
+    if (button === 'C') {
+      this.display = '';
+    } else if (button === '=') {
+      try {
+        this.display = eval(this.display).toString();
+      } catch (error) {
+        this.display = 'Error';
+      }
+    } else if (button === '+') {
+      this.display += '+';
+    } else if (button === '-') {
+      this.display += '-';
+    } else if (button === '*') {
+      this.display += '*';
+    } else if (button === '/') {
+      this.display += '/';
+    } else {
+      this.display += button;
+    }
+  }
 }
